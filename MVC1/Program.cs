33b0826821e032442343f3291098cf28e931da89
PoulaIbrahim <poulaibrahim45@gmail.com>
2026-05-22 19:07:23 +0300

    MVC 2

--- MVC1/Program.cs
+++ MVC1/Program.cs
@@ -1,3 +1,8 @@
+using GymManagement.DAL.Repositories.Classes;
+using GymManagement.DAL.Repositories.Interfaces;
+using GymMangement.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace GymMangement
 {
     public class Program
@@ -8,6 +13,12 @@ namespace GymMangement
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IPlanRepository, PlanRepository>();
+            //builder.Services.AddScoped<GymDbContext>();
+            builder.Services.AddDbContext<GymDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
